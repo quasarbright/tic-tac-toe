9cdea06aa17a8347537851606d9ddb103d2e20f4
Mike Delmonaco <mdelmonacochs@gmail.com>
2024-02-25 17:29:12 -0500

    add deps to info.rkt

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "tic-tac-toe")
-(define deps '("base" "gui-easy"))
+(define deps '("base" "gui-easy" "nat-traversal"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/tic-tac-toe.scrbl" ())))
 (define pkg-desc "Online tic tac toe")
